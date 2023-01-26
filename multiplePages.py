aa80db8dccdf9e71f5b4efd53dbc7b48e619cd4c
ryan2625 <90398601+ryan2625@users.noreply.github.com>
2023-01-25 18:04:29 -0600

    Add files via upload
    This is the gui =)

---
+++ multiplePages.py
@@ -0,0 +1,328 @@
+from tkinter import *
+from PIL import Image, ImageTk, ImageFilter
+import time
+
+
+
+class tapStrapGUI:
+
+    indicatorClicked = None
+    isClicked = True
+    count = 0
+    countBlur = 1
+    deviceConnecteed = False
+
+    def __init__(self):
+        self.startWindow1()
+        self.featuresFrame()
+        self.mainFramemainCanvas()
+        self.root.mainloop()
+    
+    def startWindow1(self):
+        self.root = Tk()
+        self.root.geometry("1000x600")
+        self.root.title("TapStrap GUI")
+        self.root.configure(bg = '#40495c' )
+        self.root.bind('<Motion>', self.eventListenerPosition)
+    #Options frame
+    def featuresFrame(self):
+        self.frame1 = Frame(self.root, bg = '#0d0d0d')
+        self.frame1.pack(side = LEFT)
+        self.frame1.pack_propagate(False)
+        self.frame1.configure(width = 175, height = 600)
+        self.featureButtons()
+    #Main Frame
+    def mainFramemainCanvas(self):
+        self.mainFrame = Frame(self.root, bg = '#40495c', highlightbackground = "blue",
+        highlightthickness= 5)
+        self.mainFrame.pack(side = LEFT)
+        self.mainFrame.pack_propagate(False)
+        self.mainFrame.configure(height = 600, width = 1000)
+    #Side Bar feature buttons
+    def featureButtons(self):
+
+        self.labelb0 = Label(text = "")
+        self.labelb0.config(bg = "#ffffff")
+        self.labelb0.place(x = 0, y = (50), width = 175, height = 2)
+        self.button1 = Button(self.frame1, text = "Feature #1",
+        font = ('Courier New', 15), fg= "White", bd = 0, bg = "#0d0d0d",
+                command = lambda: self.selectIndicator(self.button1Indicator, self.button1))
+        self.button1.place(x = 20, y = 100)
+        self.button1Indicator = Label(self.frame1, text = '', bg = "#0d0d0d")
+        self.button1Indicator.place(x = 15, y = 100, width = 5, height = 35)
+        self.labelb1 = Label(text = "")
+        self.labelb1.config(bg = "#ffffff")
+        self.labelb1.place(x = 0, y = (175), width = 175, height = 2)
+        
+
+        self.button2 = Button(self.frame1, text = "Feature #2",
+        font = ('Courier New', 15), fg= "White", bd = 0, bg = "#0d0d0d",
+                command = lambda: self.selectIndicator(self.button2Indicator, self.button2))
+        self.button2.place(x = 20, y = 225)
+        self.button2Indicator = Label(self.frame1, text = '', bg = "#0d0d0d")
+        self.button2Indicator.place(x = 15, y = 225, width = 5, height = 35)
+        self.labelb2 = Label(text = "")
+        self.labelb2.config(bg = "#ffffff")
+        self.labelb2.place(x = 0, y = (300), width = 175, height = 2)
+       
+        self.button3 = Button(self.frame1, text = "Feature #3",
+        font = ('Courier New', 15), fg= "White", bd = 0, bg = "#0d0d0d",
+                command = lambda: self.selectIndicator(self.button3Indicator, self.button3))
+        self.button3.place(x = 20, y = 350)
+        self.button3Indicator = Label(self.frame1, text = '', bg = "#0d0d0d")
+        self.button3Indicator.place(x = 15, y = 350, width = 5, height = 35)
+        self.labelb3 = Label(text = "")
+        self.labelb3.config(bg = "#ffffff")
+        self.labelb3.place(x = 0, y = (425), width = 175, height = 2)
+
+
+        self.button4 = Button(self.frame1, text = "Feature #4",
+        font = ('Courier New', 15), fg= "White", bd = 0, bg = "#0d0d0d",
+                command = lambda: self.selectIndicator(self.button4Indicator, self.button4))
+        self.button4.place(x = 20, y = 475)
+        self.button4Indicator = Label(self.frame1, text = '', bg = "#0d0d0d")
+        self.button4Indicator.place(x = 15, y = 475, width = 5, height = 35)
+        self.labelb4 = Label(text = "")
+        self.labelb4.config(bg = "#ffffff")
+        self.labelb4.place(x = 0, y = (550), width = 175, height = 2)
+    #The button Variable here is actually a label. No I won't chage it EDIT: I changed it 
+
+    def selectIndicator(self, indicator, button):
+        if (self.isClicked == True):
+            indicator.config(bg = "#ffffff")
+            self.indicatorClicked = indicator
+            self.isClicked = False
+            self.stringVar1 = button.cget('text')
+            self.text1 = self.button1.cget('text')
+            self.text2 = self.button2.cget('text')
+            self.text3 = self.button3.cget('text')
+            #which window to trigger....
+            match(self.stringVar1):
+                case self.text1:
+                    self.triggerFeat1(self.stringVar1);
+                case self.text2:
+                    self.triggerFeat2(self.stringVar1)
+                case self.text3:
+                    self.triggerFeat3(self.stringVar1)
+                case _:
+                    self.triggerFeat4(self.stringVar1)
+        else:
+            self.indicatorClicked.config(bg = '#0d0d0d')
+            indicator.config(bg = "#ffffff")
+            self.indicatorClicked = indicator
+            self.stringVar1 = button.cget('text')
+            self.destroyYourChildren(self.mainFrame)
+            match(self.stringVar1):
+                case self.text1:
+                    self.triggerFeat1(self.stringVar1);
+                case self.text2:
+                    self.triggerFeat2(self.stringVar1)
+                case self.text3:
+                    self.triggerFeat3(self.stringVar1)
+                case _:
+                    self.triggerFeat4(self.stringVar1)
+
+    def triggerFeat1(self, labelTxt):
+         self.frameFF1 = Frame(self.mainFrame, height = 600,
+         width = 1000, bg = "#40495c", highlightcolor = "blue",highlightbackground= "blue")
+         self.frameFF1.pack()
+         self.label1A= Label(self.frameFF1, text = labelTxt, bd = 5,
+         font = ("Courier New", 18), relief = RAISED, bg = "#0d0d0d",
+         fg = '#fff')
+         self.label1A.grid(row = 0, column = 1,pady = 50)
+         self.t1A = Text(self.frameFF1,  height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t1A.grid(row = 1, column = 0, padx = 25)
+         self.t2A = Text(self.frameFF1,  height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t2A.grid(row = 1, column = 1, padx = 25)
+         self.t3A = Text(self.frameFF1,  height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t3A.grid(row = 1, column = 2, padx = 25)
+         self.mainCanvas = Canvas(self.mainFrame, height = 300,
+         width = 400,bg = "black")
+         self.mainCanvas.pack(pady = 50)
+         self.backgroundImg = Image.open('wheels2.png')
+         self.tempImg = self.backgroundImg
+         print(self.backgroundImg.mode)
+         resized = self.backgroundImg.resize((200, 200), Image.LANCZOS)
+         self.imageX = ImageTk.PhotoImage(resized)
+         self.mainCanvas.create_image(213, 130, image= self.imageX)
+         self.buttonTemp = Button(master = self.mainFrame, text = "CHANGE COLOR", command = lambda : self.pillowColorCommand(self.tempImg, self.mainCanvas))
+         self.buttonTemp.place(x = 95, y = 400)
+
+    def triggerFeat2(self, labelTxt):
+         self.frameF2 = Frame(self.mainFrame, height = 600,
+         width = 1000, bg = "#40495c")
+         self.frameF2.pack()
+         self.label2A = Label(self.frameF2, text = labelTxt, bd = 5,
+         font = ("Courier New", 18), relief = RAISED, bg = "#0d0d0d",
+         fg = '#fff')
+         self.label2A.grid(row = 0, column = 1,pady = 50)
+         self.t1A = Text(self.frameF2, height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t1A.grid(row = 1, column = 0, padx = 25)
+         self.t2A = Text(self.frameF2, height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t2A.grid(row = 1, column = 1, padx = 25)
+         self.t3A = Text(self.frameF2, height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t3A.grid(row = 1, column = 2, padx = 25)
+         self.mainCanvas = Canvas(self.mainFrame, height = 300,
+         width = 400,bg = "black")
+         self.mainCanvas.pack(pady = 50)
+         self.backgroundImg = Image.open('mona1.PNG')
+         self.tempImg = self.backgroundImg
+         print(self.backgroundImg.mode)
+         resized = self.backgroundImg.resize((200, 200), Image.LANCZOS)
+         self.imageX = ImageTk.PhotoImage(resized)
+         self.mainCanvas.create_image(213, 130, image= self.imageX)
+         self.buttonTemp = Button(master = self.mainFrame, text = "BLUR", command = lambda : self.pillowBlurCommand(resized, self.mainCanvas))
+         self.buttonTemp.place(x = 85, y = 370)
+         self.buttonTemp2 = Button(master = self.mainFrame, text = "CONTNUOUS BLUR", command = lambda : self.pillowContBlur(resized, self.mainCanvas))
+         self.buttonTemp2.place(x = 85, y = 400)
+         self.buttonTemp3 = Button(master = self.mainFrame, text = "New Window", command = lambda : self.openNewWindow(self.mainCanvas))
+         self.buttonTemp3.place(x = 85, y = 430)
+         self.buttonTemp4 = Button(master = self.mainFrame, text = "Change Hue?", command = lambda : self.pillowContBlur2(resized, self.mainCanvas))
+         self.buttonTemp4.place(x = 85, y = 460)
+
+    def triggerFeat3(self, labelTxt):
+         self.frameF3 = Frame(self.mainFrame, height = 600,
+         width = 1000, bg = "#40495c")
+         self.frameF3.pack()
+         self.label3A = Label(self.frameF3, text = labelTxt, bd = 5,
+         font = ("Courier New", 18), relief = RAISED, bg = "#0d0d0d",
+         fg = '#fff')
+         self.label3A.grid(row = 0, column = 1,pady = 100)
+         self.t1A = Text(self.frameF3,  height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t1A.grid(row = 1, column = 0, padx = 25)
+         self.t2A = Text(self.frameF3, height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t2A.grid(row = 1, column = 1, padx = 25)
+         self.t3A = Text(self.frameF3, height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t3A.grid(row = 1, column = 2, padx = 25)
+
+    def triggerFeat4(self, labelTxt):
+         self.frameF4 = Frame(self.mainFrame, height = 650,
+         width = 1000, bg = "#40495c")
+         self.frameF4.pack()
+         self.label4A = Label(self.frameF4, text = labelTxt, bd = 5,
+         font = ("Courier New", 18), relief = RAISED, bg = "#0d0d0d",
+         fg = '#fff')
+         self.label4A.grid(row = 0, column = 1,pady = 100)
+         self.t1A = Text(self.frameF4,  height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t1A.grid(row = 1, column = 0, padx = 25)
+         self.t2A = Text(self.frameF4, height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t2A.grid(row = 1, column = 1, padx = 25)
+         self.t3A = Text(self.frameF4,  height = 5, width = 20, bd= 5, relief=SUNKEN, fg = "Blue")
+         self.t3A.grid(row = 1, column = 2, padx = 25)
+
+    def destroyYourChildren(self, frame):
+        for frame in frame.winfo_children():
+             frame.destroy()
+    #Experimental feature to determine if device is connected or not
+    def deviceConnecteed(self):
+        if (self.deviceConnected == True):
+            pass
+
+    def changeInput(self):
+        self.textbox1.config(text = f"Data point number {self.i}")
+        self.root.update_idletasks()
+        self.i += 1
+    #AnRGB image contains 3 bands- ‘R’ (Red), ‘G’ (Green), and ‘B’ (Blue).
+    #This method splits the RBG PNG into the 3 bands and displays them 
+    #seperately 
+    def pillowColorCommand(self, image, canvas):
+        self.destroyYourChildren(canvas)
+        arrVal = Image.Image.split(image)
+        var1 = (self.count % 4)
+        self.count +=1
+        print(var1)
+        match (var1):
+            case (1):
+                resozed= arrVal[2].resize((200,200), Image.LANCZOS)
+                self.newImage = ImageTk.PhotoImage(resozed)
+                self.mainCanvas.create_image(213, 130, image= self.newImage)
+                newLabel = Label(master = canvas, bg = "#0bed07", width = 7, height = 13)
+                newLabel.place(x = 30, y = 30)
+            case (0):
+                resozed= arrVal[1].resize((200,200), Image.LANCZOS)
+                self.newImage = ImageTk.PhotoImage(resozed)
+                self.mainCanvas.create_image(213, 130, image= self.newImage)
+                newLabel = Label(master = canvas, bg = "Red", width = 7, height = 13)
+                newLabel.place(x = 30, y = 30)
+            case (2):
+                resozed= arrVal[0].resize((200,200), Image.LANCZOS)
+                self.newImage = ImageTk.PhotoImage(resozed)
+                self.mainCanvas.create_image(213, 130, image= self.newImage)
+                newLabel = Label(master = canvas, bg = "Blue", width = 7, height = 13)
+                newLabel.place(x = 30, y = 30)
+            case (3):
+                resozed = image.resize((200,200), Image.LANCZOS)
+                self.newImage = ImageTk.PhotoImage(resozed)
+                self.mainCanvas.create_image(213, 130, image= self.newImage)\
+    #Debugger printing cursor location on parent widget.
+    #Useful for finding borders of frames/canvas'and placing
+    #widgets =)ds21
+
+    def eventListenerPosition(self, event):
+        x, y = event.x, event.y
+        print('{}, {}'.format(x, y))
+
+    #Button to blur discretely
+    def pillowBlurCommand(self, image1, canvas):
+        if (self.countBlur % 3 == 1):
+            self.destroyYourChildren(canvas)
+            self.newImage1 = ImageTk.PhotoImage(image1.filter(ImageFilter.GaussianBlur(4)))
+            canvas.create_image(213, 130, image = self.newImage1)
+            self.countBlur +=1
+        elif(self.countBlur % 3 == 0 ):
+            self.destroyYourChildren(canvas)
+            self.newImage1 = ImageTk.PhotoImage(image1)
+            canvas.create_image(213, 130, image = self.newImage1)
+            self.countBlur +=1
+        else:
+            self.destroyYourChildren(canvas)
+            self.newImage1 = ImageTk.PhotoImage(image1.filter(ImageFilter.GaussianBlur(13)))
+            canvas.create_image(213, 130, image = self.newImage1)
+            self.countBlur +=1
+    
+    def pillowContBlur(self, image1, canvas):
+        #Increase Gaussian Blur
+        for x in range(0, 30):
+            self.newImage2 = ImageTk.PhotoImage(image1.filter(ImageFilter.GaussianBlur(x)))
+            canvas.create_image(213, 130, image = self.newImage2)
+            self.destroyYourChildren(canvas)
+            canvas.update_idletasks()
+            time.sleep(.05)
+          #  print(x)
+        #epic jumpsare
+        #self.tempImg = image1.resize((400, 300), Image.LANCZOS)
+        # self.newImage3 = ImageTk.PhotoImage(image1.filter((ImageFilter.FIND_EDGES)))
+        # canvas.create_image(213, 130, image = self.newImage3)
+        # canvas.update_idletasks()
+        # time.sleep(.05)
+        # canvas.delete("all")
+        #Reduce Gaussian Blur
+        for j in reversed(range(0,30)):
+            self.newImage2 = ImageTk.PhotoImage(image1.filter(ImageFilter.GaussianBlur(j)))
+            canvas.create_image(213, 130, image = self.newImage2)
+            self.destroyYourChildren(canvas)
+            canvas.update_idletasks()
+            time.sleep(.03)
+        #    print(j)
+        # ADD FUNCTIONALITYYYY
+
+    def pillowContBlur2(self, image1, canvas):
+        #Increase Gaussian Blur
+        for x in range(0, 100):
+            self.newImage2 = ImageTk.PhotoImage(image1.point(lambda p: p > x and 255))
+            canvas.create_image(213, 130, image = self.newImage2)
+            self.destroyYourChildren(canvas)
+            canvas.update_idletasks()
+            time.sleep(.05)
+
+    def openNewWindow(self,pWindow):
+        newWindow = Toplevel(pWindow)
+        newWindow.title("New Window")
+        newWindow.geometry("200x200")
+        Label(newWindow,
+          text ="This is a new window").pack()
+                 
+    def pillowHueCommand(self, iamge1, canvas):
+        pass
+
+tapStrapGUI()
